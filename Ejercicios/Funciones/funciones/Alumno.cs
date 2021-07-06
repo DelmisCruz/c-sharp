2d67be2fbf9b0c81a233b3df545a7093f98159dc
Delmis Cruz <delmismarisela17@gmail.com>
2021-07-06 12:10:19 -0600

    Video 14

--- Ejercicios/Funciones/funciones/Alumno.cs
+++ Ejercicios/Funciones/funciones/Alumno.cs
@@ -2,16 +2,30 @@ public class Alumno
 {
     public int Id { get; set; }
     public string PrimerNombre { get; set; }
+
     public string SegundoNombre { get; set; }
+
+    public bool Activo { get; set; }
 
     public Alumno(int id, string primerNombre, string segundoNombre)
     {
         Id = id;
         PrimerNombre = primerNombre;
         SegundoNombre = segundoNombre;
+        Activo = true;
     }
     //Funcion 
     public string nombreCompleto(){
-        return PrimerNombre + " " + SegundoNombre;
+        if (Activo == true){
+            return PrimerNombre + " " + SegundoNombre;
+        }  
+
+        return "Alummno inactivo";
+     }
+    public void inactivarAlumno(){
+        Activo = false;
+    }
+    public void activarAlumno(){
+        Activo = true;
     }
 }
